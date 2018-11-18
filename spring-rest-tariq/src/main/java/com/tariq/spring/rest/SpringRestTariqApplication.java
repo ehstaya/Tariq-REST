b3736f532cd2f95cd5e930136f6de1cc0f90c447
Tariq Yaqub <ehstaya@gmail.com>
2018-11-17 18:12:41 -0800

    Added

---
+++ spring-rest-tariq/src/main/java/com/tariq/spring/rest/SpringRestTariqApplication.java
@@ -0,0 +1,12 @@
+package com.tariq.spring.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestTariqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestTariqApplication.class, args);
+	}
+}
